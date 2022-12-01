752e0263719373741c83a26fedc4838b1697949f
Aryan0804 <87126414+Aryan0804@users.noreply.github.com>
2022-12-01 16:57:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAwfwR1_q6bB1Qu6aF59_cfiUsD4YA4rLw",
+  authDomain: "e-library-e4ab1.firebaseapp.com",
+  projectId: "e-library-e4ab1",
+  storageBucket: "e-library-e4ab1.appspot.com",
+  messagingSenderId: "147696529527",
+  appId: "1:147696529527:web:f8601c61c27b9fe3cc1672"
+};
